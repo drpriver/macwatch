29fcf6c08b11d086b3738e6ac05980faab971a8d
drpriver <priver.david@gmail.com>
2021-08-31 00:26:52 -0400

    Handle large number of files

--- makewatch.py
+++ makewatch.py
@@ -13,28 +13,47 @@ def main() -> None:
     args = parser.parse_args()
     run(**vars(args))
 
+def line_to_dependencies(line):
+    deps = []
+    line = line[line.index(':')+1:]
+    idx = line.find('|')
+    if idx > -1:
+        line = line[:idx]
+    for dep in line.strip().split():
+        if dep.endswith('.dep'):
+            continue
+        deps.append(dep)
+    return deps
+
+def expand(targets, target):
+    deps = targets[target]
+    if not deps:
+        return [target]
+    result = []
+    for dep in deps:
+        if dep in targets:
+            result.extend(expand(targets, dep))
+        result.append(dep)
+    return result
+
 def run(target:str) -> None:
     proc = subprocess.Popen(['make', '-p', '--dry-run'], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
     rule = target + ':'
-    deps = []
+    targets = {}
     for line_ in proc.stdout:
         line = line_.decode('utf-8').strip()
         if not line:
             continue
         if line.startswith('#'):
             continue
-        if line.startswith(rule):
-            idx = line.find('|')
-            if idx > -1:
-                line = line[:idx]
-            for dep in line[len(rule):].strip().split():
-                if dep.endswith('.dep'):
-                    continue
-                deps.append(dep)
+        if ':' in line:
+            targets[line[:line.index(':')]] = line_to_dependencies(line)
+
     proc.wait()
+    true_deps = sorted(set(expand(targets, target)))
     mac = subprocess.Popen(['macwatch', f'make {target}'], stdin=subprocess.PIPE)
     try:
-        for dep in deps:
+        for dep in true_deps:
             mac.stdin.write(dep.encode('utf-8')+b'\n')
         mac.stdin.flush()
         mac.stdin.close()
