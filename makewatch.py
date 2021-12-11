0c4440fc76584d642390af7643c5f269032df404
drpriver <priver.david@gmail.com>
2021-12-11 15:25:10 -0500

    add comments

--- makewatch.py
+++ makewatch.py
@@ -6,39 +6,17 @@ of a target and only attempt to rebuild it when they change.
 import sys
 import subprocess
 import argparse
-from typing import List, Dict
+from typing import List, Dict, Tuple
 
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument('target')
     parser.add_argument('--always-make', '-B', action='store_true')
+    parser.add_argument('--depsuff', nargs='*', default=['.dep', '.d'], help='suffix for dependency files to be ignore')
     args = parser.parse_args()
     run(**vars(args))
 
-def line_to_dependencies(line:str) -> List[str]:
-    deps = []
-    line = line[line.index(':')+1:]
-    idx = line.find('|')
-    if idx > -1:
-        line = line[:idx]
-    for dep in line.strip().split():
-        if dep.endswith('.dep'):
-            continue
-        deps.append(dep)
-    return deps
-
-def expand(targets:Dict[str, List[str]], target:str) -> List[str]:
-    deps = targets[target]
-    if not deps:
-        return [target]
-    result = []
-    for dep in deps:
-        if dep in targets:
-            result.extend(expand(targets, dep))
-        result.append(dep)
-    return result
-
-def run(target:str, always_make:bool=False) -> None:
+def run(target:str, always_make:bool, depsuff:Tuple[str]) -> None:
     proc = subprocess.Popen(['make', '-p', '--dry-run'], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
     rule = target + ':'
     targets = {}
@@ -50,10 +28,11 @@ def run(target:str, always_make:bool=False) -> None:
         if line.startswith('#'):
             continue
         if ':' in line:
-            targets[line[:line.index(':')]] = line_to_dependencies(line)
+            targets[line[:line.index(':')]] = line_to_dependencies(line, depsuff)
 
     proc.wait()
     true_deps = sorted(set(expand(targets, target)))
+    # macwatch can take dependencies via stdin if invoked non-interactively.
     mac = subprocess.Popen(['macwatch', f'make {"--always-make " if always_make else ""}{target}'], stdin=subprocess.PIPE)
     try:
         assert mac.stdin
@@ -64,6 +43,39 @@ def run(target:str, always_make:bool=False) -> None:
         mac.wait()
     except KeyboardInterrupt:
         mac.kill()
+
+def line_to_dependencies(line:str, depsuff:Tuple[str]) -> List[str]:
+    '''
+    Extracts the dependency list from a target line.
+    BUG: files with spaces are incorrectly handled.
+    '''
+    deps = []
+    line = line[line.index(':')+1:]
+    # ignore order-only dependencies
+    idx = line.find('|')
+    if idx > -1:
+        line = line[:idx]
+    for dep in line.strip().split():
+        if dep.endswith(depsuff):
+            continue
+        deps.append(dep)
+    return deps
+
+def expand(targets:Dict[str, List[str]], target:str) -> List[str]:
+    '''
+    Flattens a dependency graph for a given target to a list.
+    As a special case, returns the target itself as a dependency
+    if it has no dependencies.
+    '''
+    deps = targets[target]
+    if not deps:
+        return [target]
+    result = []
+    for dep in deps:
+        if dep in targets:
+            result.extend(expand(targets, dep))
+        result.append(dep)
+    return result
 
 
 if __name__ == '__main__':
